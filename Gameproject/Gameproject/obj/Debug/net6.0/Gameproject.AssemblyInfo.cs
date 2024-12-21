65d464c22ff104e49ce4dbaf96c6ea4beb38ff99
Zouhir <s148643@ap.be>
2024-12-21 19:38:04 +0100

    enemyManager added

--- Gameproject/Gameproject/obj/Debug/net6.0/Gameproject.AssemblyInfo.cs
+++ Gameproject/Gameproject/obj/Debug/net6.0/Gameproject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gameproject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbe3a258fc78ced44d0f0a9012448de35d571a43")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c1a66d2d30bb84e47dcc6a85beaa34745d28c25")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gameproject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gameproject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
