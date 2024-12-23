23093bb4104801773aef4012d7a025d5734b239e
Zouhir <s148643@ap.be>
2024-12-24 00:28:01 +0100

    new keyboard input added spacebar

--- Gameproject/Gameproject/Input/KeyBoardReader.cs
+++ Gameproject/Gameproject/Input/KeyBoardReader.cs
@@ -10,10 +10,22 @@ namespace Gameproject.Input
 {
     internal class KeyBoardReader : IinputReader
     {
+        private bool attackpressed;
+        public bool Attackpressed
+        {
+            get
+            {
+                return attackpressed;
+            }
+        }
+
         public Vector2 ReaderInput()
         {
+
             var direction = Vector2.Zero;
+            attackpressed = false;
             KeyboardState state = Keyboard.GetState();
+
             if (state.IsKeyDown(Keys.Left))
             {
                 direction = new Vector2(-1, 0);
@@ -29,6 +41,10 @@ namespace Gameproject.Input
             if (state.IsKeyDown(Keys.Down))
             {
                 direction.Y = 1;
+            }
+            if(state.IsKeyDown(Keys.Space))
+            {
+                attackpressed = true;
             }
             return direction;
         }
