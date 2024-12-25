085d5003e218acd69602d8de0b6d57fcd20fe752
Zouhir <s148643@ap.be>
2024-12-25 01:23:54 +0100

    Startscreen Constructor made

---
+++ Gameproject/Gameproject/Screens/StartScreen.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Gameproject.Screens
+{
+    public class StartScreen
+    {
+        private Texture2D background;
+        private Texture2D button;
+        private Texture2D message;
+        private Rectangle buttonRectangle;
+        private SoundEffect music;
+        private SoundEffectInstance musicInstance;
+
+        private MouseState currentMouseState;
+        private MouseState previousMouseState;
+
+        public StartScreen(Texture2D background, Texture2D button, Texture2D message, SoundEffect music)
+        {
+            this.background = background;
+            this.button = button;
+            this.message = message;
+            this.music = music;
+
+            this.musicInstance = music.CreateInstance();
+            this.musicInstance.IsLooped = true;
+
+            this.buttonRectangle = new Rectangle(540, 360, button.Width, button.Height);
+        }
+
+    }
+}
--- Gameproject/Gameproject/obj/Release/net6.0/Gameproject.AssemblyInfo.cs
+++ Gameproject/Gameproject/obj/Release/net6.0/Gameproject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gameproject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8de742281cd2b597568af4c7620043e6667c610d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+430f09d51cd8544b2398728dae2f59f8825be313")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gameproject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gameproject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
