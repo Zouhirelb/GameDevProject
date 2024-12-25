b322f29812e139dbf3a19c347f72fbf7f383e164
Zouhir <s148643@ap.be>
2024-12-25 01:54:40 +0100

    GameOverscreen Constructor made

---
+++ Gameproject/Gameproject/Screens/GameOverScreen.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Media;
+
+namespace Gameproject.Screens
+{
+    public class GameOverScreen
+    {
+        private Texture2D background;
+        private Texture2D message;
+        private Song music;
+        private GraphicsDevice graphicsDevice;
+
+        public GameOverScreen(Texture2D background, Texture2D message, Song music, GraphicsDevice graphicsDevice)
+        {
+            this.background = background;
+            this.message = message;
+            this.music = music;
+            this.graphicsDevice = graphicsDevice;
+        }
+    }
+}
--- Gameproject/Gameproject/obj/Debug/net6.0/Gameproject.AssemblyInfo.cs
+++ Gameproject/Gameproject/obj/Debug/net6.0/Gameproject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gameproject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51ad58ff0038ce45fd11029228465df57da4999b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f4a17428a4ac488e03a96f9f9fb1d1559a68aaa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gameproject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gameproject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
