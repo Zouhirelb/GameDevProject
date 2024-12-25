847d0424d395b155142eb5ab740bf2e0fb1957da
Zouhir <s148643@ap.be>
2024-12-25 01:56:04 +0100

    GameOverscreen updated

--- Gameproject/Gameproject/Screens/GameOverScreen.cs
+++ Gameproject/Gameproject/Screens/GameOverScreen.cs
@@ -3,8 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Gameproject.Managers;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
+using static Gameproject.Managers.GameStateManager;
 
 namespace Gameproject.Screens
 {
@@ -22,5 +26,40 @@ namespace Gameproject.Screens
             this.music = music;
             this.graphicsDevice = graphicsDevice;
         }
+        public void Update()
+        {
+            if (MediaPlayer.State != MediaState.Playing)
+            {
+                MediaPlayer.Play(music);
+                MediaPlayer.IsRepeating = true;
+            }
+
+            
+            if (Keyboard.GetState().IsKeyDown(Keys.Enter))
+            {
+                GameStateManager.CurrentState = GameState.StartScreen;
+                MediaPlayer.Stop(); 
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Begin();
+
+            spriteBatch.Draw(
+                background,
+                new Rectangle(0, 0, graphicsDevice.Viewport.Width, graphicsDevice.Viewport.Height),
+                Color.White
+            );
+
+            Vector2 messagePosition = new Vector2(
+                (graphicsDevice.Viewport.Width - message.Width) / 2,
+                (graphicsDevice.Viewport.Height - message.Height) / 2
+            );
+            spriteBatch.Draw(message, messagePosition, Color.White);
+
+            spriteBatch.End();
+        }
     }
+}
 }
