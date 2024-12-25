51ad58ff0038ce45fd11029228465df57da4999b
Zouhir <s148643@ap.be>
2024-12-25 01:25:24 +0100

    Startscreen updated

--- Gameproject/Gameproject/Screens/StartScreen.cs
+++ Gameproject/Gameproject/Screens/StartScreen.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Gameproject.Managers;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using static Gameproject.Managers.GameStateManager;
 
 namespace Gameproject.Screens
 {
@@ -33,6 +35,32 @@ namespace Gameproject.Screens
             this.musicInstance.IsLooped = true;
 
             this.buttonRectangle = new Rectangle(540, 360, button.Width, button.Height);
+        }
+        public void Update()
+        {
+            currentMouseState = Mouse.GetState();
+
+            if (currentMouseState.LeftButton == ButtonState.Pressed &&
+                previousMouseState.LeftButton == ButtonState.Released &&
+                buttonRectangle.Contains(currentMouseState.Position))
+            {
+                GameStateManager.CurrentState = GameState.Playing;
+                musicInstance.Stop();
+            }
+
+            if (musicInstance.State != SoundState.Playing)
+                musicInstance.Play();
+
+            previousMouseState = currentMouseState;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Begin();
+            spriteBatch.Draw(background, Vector2.Zero, Color.White);
+            spriteBatch.Draw(button, buttonRectangle, Color.White);
+            spriteBatch.Draw(message, new Vector2(400, 200), Color.White);
+            spriteBatch.End();
         }
 
     }
